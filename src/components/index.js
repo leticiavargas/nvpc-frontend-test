12b73415ecc971956410a1813f2f1536ea07e623
leticia.vargas <le.mvargas@gmail.com>
2023-03-16 00:57:42 -0300

    Update list of components

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,17 @@
+import Button from "./Button";
 import ListRow from "./ListRow";
 import RepositoriesList from "./RepositoriesList";
+import SearchBar from "./SearchBar";
+import SelectButton from "./SelectButton";
+import SelectMenu from "./SelectMenu";
 import Tag from "./Tag";
 
 export {
+  Button,
   ListRow,
   RepositoriesList,
+  SearchBar,
+  SelectButton,
+  SelectMenu,
   Tag
 }
